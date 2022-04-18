7e687806645e9c0db6c86a06762298eab872ed2d
Joey Schroeder <schrjos@gmail.com>
2022-04-18 09:44:44 -0500

    feat: Add AppBar component

---
+++ src/components/app/app-bar/app-bar.js
@@ -0,0 +1,42 @@
+import { Menu } from '@mui/icons-material';
+import {
+  AppBar as MaterialAppBar,
+  IconButton,
+  Toolbar,
+  Typography,
+} from '@mui/material';
+import PropTypes from 'prop-types';
+import React from 'react';
+
+function AppBar(props) {
+  const { onMenuClick, title } = props;
+
+  return (
+    <MaterialAppBar>
+      <Toolbar>
+        <IconButton
+          color="inherit"
+          edge="start"
+          onClick={onMenuClick}
+          size="large"
+          sx={{ mr: 1 }}
+        >
+          <Menu />
+        </IconButton>
+        {title && <Typography variant="h6">{title}</Typography>}
+      </Toolbar>
+    </MaterialAppBar>
+  );
+}
+
+AppBar.propTypes = {
+  onMenuClick: PropTypes.func,
+  title: PropTypes.string,
+};
+
+AppBar.defaultProps = {
+  onMenuClick: undefined,
+  title: '',
+};
+
+export default AppBar;
