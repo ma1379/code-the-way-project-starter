41dba53ed367fdb355583f0ca34120f0f2437321
Joey Schroeder <schrjos@gmail.com>
2022-04-18 12:16:32 -0500

    Add DemoForm reducer

--- src/store/create-root-reducer.js
+++ src/store/create-root-reducer.js
@@ -1,8 +1,13 @@
 import { combineReducers } from 'redux';
+import {
+  demoFormReducer,
+  REDUCER_PREFIX as DEMO_FORM,
+} from './demo-form/demo-form';
 import { sampleReducer, REDUCER_PREFIX as SAMPLE } from './sample/sample';
 
 export function createRootReducer() {
   return combineReducers({
     [SAMPLE]: sampleReducer,
+    [DEMO_FORM]: demoFormReducer,
   });
 }
