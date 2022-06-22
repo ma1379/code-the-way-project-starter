bc39c8ac6bc372004d1be667c449dad05dad1785
Puj Manohar <23manohp@elmbrookstudents.org>
2022-06-22 13:42:16 -0500

    Jason stuff done

---
+++ src/components/waste/homepage.js
@@ -0,0 +1,90 @@
+import Container from '@mui/material/Container';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
+import { useNavigate } from 'react-router-dom';
+import Link from '@mui/material/Link';
+import ROUTES from '../../constants/routes';
+
+export default function Home() {
+  const navigate = useNavigate();
+
+  const onRepositoryClick = () => {
+    window.open(
+      'https://github.com/joeyschroeder/code-the-way-project-starter'
+    );
+  };
+
+  const onDemoSignUpFormClick = () => {
+    navigate(ROUTES.SIGN_UP);
+  };
+
+  const onStudentListClick = () => {
+    navigate(ROUTES.STUDENTS);
+  };
+
+  return (
+    <Container maxWidth="sm">
+      <Box
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+        }}
+      >
+        <Typography
+          align="center"
+          color="text.primary"
+          component="h1"
+          gutterBottom
+          variant="h2"
+          sx={{ mt: 5 }}
+        >
+          Welcome to the Waste Management Company ! 
+        </Typography>
+        <Typography
+          align="center"
+          color="text.secondary"
+          paragraph
+          variant="h5"
+        >
+          We are have two on-site locations in {' '}
+          <Link
+            color="primary"
+            href="https://reactjs.org/docs/getting-started.html"
+            target="_blank"
+          >
+            Brookfield
+          </Link>
+          , and{' '}
+          <Link
+            color="primary"
+            href="https://mui.com/material-ui/getting-started"
+            target="_blank"
+          >
+            Milwaukee.
+          </Link>
+          . It&apos;s 
+        </Typography>
+        <Stack
+          sx={{ pt: 4 }}
+          direction="row"
+          spacing={2}
+          justifyContent="center"
+        >
+          <Button variant="contained" onClick={onDemoSignUpFormClick}>
+            Demo Sign-Up Form
+          </Button>
+
+          <Button variant="outlined" onClick={onRepositoryClick}>
+            Repository
+          </Button>
+          <Button variant="contained" onClick={onStudentListClick}>
+            Students List
+          </Button>
+        </Stack>
+      </Box>
+    </Container>
+  );
+}
