eafd26b4a6f77c3fdb66299935c2dcc74c1f3f6c
Sanjay Murali <muralisanjay961@gmail.com>
2022-06-23 10:57:06 -0500

    working on delete waste more

--- src/components/waste/deleteItems.js
+++ src/components/waste/deleteItems.js
@@ -1,14 +1,17 @@
-import Button from '@mui/material/Button';
-import Grid from '@mui/material/Grid';
+import Container from '@mui/material/Container';
+import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
-import TextField from '@mui/material/TextField';
-import PropTypes from 'prop-types';
-import { Dialog, DialogTitle } from '@mui/material';
 import { useState, useEffect } from 'react';
+import AddStudent from './student-forms/add-waste';
+import StudentsList from './waste-list';
+import UpdateStudent from './student-forms/update-waste';
+import {
+  getWaste,
+  addWaste,
+  updateWaste,
+} from '../../services/services';
 
-export default function DeleteWastes(props) {
-  const { waste, onSubmit, handleClose } = props;
-
+export default function Students() {
   const [name, setName] = useState('');
   const [owner, setOwner] = useState('');
   const [price, setPrice] = useState('');
@@ -17,119 +20,129 @@ export default function DeleteWastes(props) {
   const [postalCode, setPostalCode] = useState('');
 
   useEffect(() => {
-    initializeForm();
+    refreshStudents();
   }, []);
 
-  const initializeForm = () => {
-    if (student.studentFirstName != null) {
-      setFirstName(student.studentFirstName);
-    }
-    if (student.studentLastName != null) {
-      setLastName(student.studentLastName);
-    }
-    if (student.studentDateOfBirth != null) {
-      const date = student.studentDateOfBirth.substring(0, 10);
-      setBirthDate(date);
-    }
-    if (student.studentCellPhone != null) {
-      setCellPhone(student.studentCellPhone);
-    }
-    if (student.studentEmail != null) {
-      setEmailAddress(student.studentEmail);
+  const refreshStudents = async () => {
+    const response = await getStudents();
+    setStudents(response);
+    setActiveStudents(response.filter((student) => student.active === true));
+  };
+
+  const onAddStudentFormSubmit = async (
+    enteredFirstName,
+    enteredLastName,
+    enteredBirthDate,
+    enteredCellPhone,
+    enteredEmailAddress
+  ) => {
+    const newStudent = {
+      studentFirstName: enteredFirstName,
+      studentLastName: enteredLastName,
+      studentDateOfBirth: enteredBirthDate,
+      studentCellPhone: enteredCellPhone,
+      studentEmail: enteredEmailAddress,
+      active: true,
+    };
+
+    await addStudent(newStudent);
+    refreshStudents();
+    setFirstName('');
+    setLastName('');
+    setBirthDate('');
+    setCellPhone('');
+    setEmailAddress('');
+  };
+
+  const archiveStudentHandler = async (studentId) => {
+    const updatedStudent = {
+      id: studentId,
+      Active: false,
+    };
+    await updateStudent(updatedStudent);
+    refreshStudents();
+  };
+
+  const updateModalChange = (studentId) => {
+    if (updateStudentModal === true) {
+      setUpdateStudentModal(false);
+    } else {
+      const studentIndex = students.findIndex(
+        (student) => student.id === studentId
+      );
+      setStudentToUpdate(students[studentIndex]);
+      setUpdateStudentModal(true);
     }
   };
 
-  const updateStudentHandler = (event) => {
-    event.preventDefault();
-    onSubmit(
-      student.id,
-      firstName,
-      lastName,
-      birthDate,
-      cellPhone,
-      emailAddress
-    );
-  };
-
-  const firstNameChangeHandler = (event) => {
-    setFirstName(event.target.value);
-  };
-  const lastNameChangeHandler = (event) => {
-    setLastName(event.target.value);
-  };
-  const birthDateChangeHandler = (event) => {
-    setBirthDate(event.target.value);
-  };
-  const cellPhoneChangeHandler = (event) => {
-    setCellPhone(event.target.value);
-  };
-  const emailAddressChangeHandler = (event) => {
-    setEmailAddress(event.target.value);
+  const updateStudentHandler = async (
+    studentId,
+    newFirstName,
+    newLastName,
+    newBirthDate,
+    newCellPhone,
+    newEmailAddress
+  ) => {
+    const updatedStudent = {
+      id: studentId,
+      studentFirstName: newFirstName,
+      studentLastName: newLastName,
+      studentDateOfBirth: newBirthDate,
+      studentCellPhone: newCellPhone,
+      studentEmail: newEmailAddress,
+      active: true,
+    };
+    await updateStudent(updatedStudent);
+    refreshStudents();
+    updateModalChange();
   };
 
   return (
-    <Dialog open onClose={handleClose}>
-      <DialogTitle>Update Student</DialogTitle>
-      <Box>
-        <Grid container spacing={2}>
-          <Grid item xs={12} sm={6}>
-            <TextField
-              fullWidth
-              label="First Name"
-              onChange={firstNameChangeHandler}
-              value={firstName}
-            />
-          </Grid>
-          <Grid item xs={12} sm={6}>
-            <TextField
-              fullWidth
-              label="Last Name"
-              onChange={lastNameChangeHandler}
-              value={lastName}
-            />
-          </Grid>
-          <Grid item xs={12}>
-            <TextField
-              fullWidth
-              label="Date of Birth"
-              onChange={birthDateChangeHandler}
-              value={birthDate}
-            />
-          </Grid>
-          <Grid item xs={12}>
-            <TextField
-              fullWidth
-              label="Cell Phone Number"
-              onChange={cellPhoneChangeHandler}
-              value={cellPhone}
-            />
-          </Grid>
-          <Grid item xs={12}>
-            <TextField
-              fullWidth
-              label="Email Address"
-              onChange={emailAddressChangeHandler}
-              type="email"
-              value={emailAddress}
-            />
-          </Grid>
-        </Grid>
-        <Button
-          fullWidth
-          sx={{ mt: 3, mb: 2 }}
-          type="submit"
-          onClick={updateStudentHandler}
-          variant="contained"
-        >
-          Update Student
-        </Button>
+    <Container maxWidth="sm">
+      {updateStudentModal && (
+        <UpdateStudent
+          student={studentToUpdate}
+          onSubmit={updateStudentHandler}
+          handleClose={updateModalChange}
+        />
+      )}
+      <Box
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+        }}
+      >
+        <Typography component="h1" variant="h5">
+          Students
+        </Typography>
+        <AddStudent
+          firstName={firstName}
+          lastName={lastName}
+          birthDate={birthDate}
+          cellPhone={cellPhone}
+          emailAddress={emailAddress}
+          onFirstNameChange={setFirstName}
+          onLastNameChange={setLastName}
+          onBirthDateChange={setBirthDate}
+          onCellPhoneChange={setCellPhone}
+          onEmailAddressChange={setEmailAddress}
+          onSubmit={onAddStudentFormSubmit}
+        />
       </Box>
-    </Dialog>
+      <Box
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+        }}
+      >
+        <StudentsList
+          students={activeStudents}
+          archiveStudentHandler={archiveStudentHandler}
+          updateStudentHandler={updateModalChange}
+        />
+      </Box>
+    </Container>
   );
 }
-
-UpdateStudent.propTypes = {
-  student: PropTypes.object.isRequired,
-  onSubmit: PropTypes.func.isRequired,
-  handleClose: PropTypes.func.isRequired,
-};
