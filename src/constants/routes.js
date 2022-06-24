431278f2712668a8bd7d6fcfad7830169e26e8c9
Puj Manohar <23manohp@elmbrookstudents.org>
2022-06-24 09:23:00 -0500

    ProgressMade

--- src/constants/routes.js
+++ src/constants/routes.js
@@ -2,7 +2,7 @@ const ROUTES = {
   HOMEPAGE: '/homepage',
   SIGN_UP: '/sign-up',
   WASTES: '/wastes',
-  SIGN_UP_SUCCESS: '/sign-up-succes',
+  SIGN_UP_SUCCESS: '/sign-up-success',
 };
 
 export default ROUTES;
