c95b286c5ac24c0c2fa6faaf582c0b43e67b9814
Sanjay Murali <muralisanjay961@gmail.com>
2022-07-11 13:45:23 -0500

    Finished first part of questions

--- src/components/interview/InterviewQuestions.js
+++ src/components/interview/InterviewQuestions.js
@@ -1,21 +1,51 @@
 import React from 'react';
 import TextField from "@mui/material/TextField";
-import RadioButton from "@mui/material/RadioButton";
+import Radio from "@mui/material/Radio";
+import Box from '@mui/material/Box';
+import Container from "@mui/material/Container";
+import Divider from "@mui/material/Divider";
 
 export default function InterviewQuestions() {
     return (
-    <h2 align = "center">'Interview Questions'</h2>;
-    <body>
-        <text>'On your application, you stated that you would like to be a ______________, why did you choose this career?'</text>;
-        <TextField id="filled-required" label="" variant="filled" />;
-        <text>'Describe your personal strengths? Why do you describe those as strengths?'</text>;
-        <TextField id="filled-required" label="" variant="filled" />;
-        <text>'Of the following words - Commitment, Leadership, and Achievement; which is most important? Why is it important to you?'</text>;
-        <TextField id="filled-required" label="" variant="filled" />;
-        <text>'Describe a difficult situation/project and how you overcame it?'</text>;
-        <TextField id="filled-required" label="" variant="filled" />;
-        <text>'What other extra-curricular activities, employment opportunities or other programs are you currently involved in or will you become involved in this year?'</text>;
-        <TextField id="filled-required" label="" variant="filled" />;
-    </body>
+        <Container>
+            <Box components="span" sx={{
+                p: 2,
+                width: 700,
+                height: 200,
+            }}> On your application, you stated that you would like to be a ______________, why did you choose this career?
+                <TextField id='filled-required' label='' variant='filled' fullWidth sx={{ m: 5 }} />;
+            </Box>
+            <Box components="span" sx={{
+                p: 2,
+                width: 700,
+                height: 200,
+            }}> Describe your personal strengths? Why do you describe those as strengths?
+                <TextField id='filled-required' label='' variant='filled' fullWidth sx={{ m: 5 }} />;
+            </Box>
+            <Box components="span" sx={{
+                p: 2,
+                width: 700,
+                height: 200,
+            }}> Of the following words - Commitment, Leadership, and Achievement; which is most important? Why is it important to you?
+                <TextField id='filled-required' label='' variant='filled' fullWidth sx={{ m: 5 }} />;
+            </Box>
+            <Box components="span" sx={{
+                p: 2,
+                width: 700,
+                height: 200,
+            }}> Describe a difficult situation/project and how you overcame it?
+                <TextField id='filled-required' label='' variant='filled' fullWidth sx={{ m: 5 }} />;
+            </Box>
+            <Box components="span" sx={{
+                p: 2,
+                width: 700,
+                height: 200,
+            }}> What other extra-curricular activities, employment opportunities or other programs are you currently involved in or will you become involved in this year?
+                <TextField id='filled-required' label='' variant='filled' fullWidth sx={{ m: 5 }} />;
+            </Box>
+            <Divider>In the remaining questions, check only one answer</Divider>
+
+        </Container>
     );
 }
+
