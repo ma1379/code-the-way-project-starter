40966a5beeed97bbbf1fbd152296cc39e84208c2
pujamanohaaaar <107407712+pujamanohaaaar@users.noreply.github.com>
2022-07-18 09:32:53 -0500

    Grit Radio Buttons Implimented

--- src/components/interview/GritQuestions.js
+++ src/components/interview/GritQuestions.js
@@ -1,30 +1,14 @@
 import React from 'react';
 import Box from '@mui/material/Box';
-import Slider from '@mui/material/Slider';
 import Container from '@mui/material/Container';
+import Radio from '@mui/material/Radio';
+import RadioGroup from '@mui/material/RadioGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import FormControl from '@mui/material/FormControl';
+import FormLabel from '@mui/material/FormLabel';
 
-const marks = [
-  {
-    value: 0,
-    label: 'Not Like Me At All',
-  },
-  {
-    value: 25,
-    label: 'Not Much Like Me',
-  },
-  {
-    value: 50,
-    label: 'Somewhat Like Me',
-  },
-  {
-    value: 75,
-    label: 'Mostly Like Me',
-  },
-  {
-    value: 100,
-    label: 'Very Much Like Me',
-  },
-];
+
+
 
 export default function GritSliderQuestions() {
   return (
@@ -33,171 +17,196 @@ export default function GritSliderQuestions() {
         components="span"
         sx={{
           px: 10,
-          py: 5,
+          py: 2,
           width: '100%',
           height: '5%',
         }}
       >
-        {' '}
-        1. New ideas and projects sometimes distract me from previous ones.
-        <Slider
-          // aria-label="Restricted values"
-          defaultValue={50}
-          step={null}
-          marks={marks}
-          valueLabelDisplay="off"
-          track={false}
-        />
+        <FormControl>
+  <FormLabel id="demo-controlled-radio-buttons-group">1. New ideas and projects sometimes distract me from previous ones. </FormLabel>
+  <RadioGroup
+    aria-labelledby="demo-controlled-radio-buttons-group"
+    name="controlled-radio-buttons-group"
+
+  >
+    <FormControlLabel value="1" control={<Radio />} label="Very Much Like Me" />
+    <FormControlLabel value="2" control={<Radio />} label="Mostly Like Me" />
+    <FormControlLabel value="3" control={<Radio />} label="Somewhat Like Me" />
+    <FormControlLabel value="4" control={<Radio />} label="Not Much Like Me" />
+    <FormControlLabel value="5" control={<Radio />} label="Not Like Me At All" />
+  </RadioGroup>
+</FormControl>
+
+</Box>
+
+      <Box
+        components="span"
+        sx={{
+          px: 10,
+          py: 2,
+          width: '100%',
+          height: '5%',
+        }}
+      ><FormControl>
+      <FormLabel id="demo-controlled-radio-buttons-group">2. Setbacks do not discourage me. </FormLabel>
+      <RadioGroup
+        aria-labelledby="demo-controlled-radio-buttons-group"
+        name="controlled-radio-buttons-group"
+    
+      >
+        <FormControlLabel value="1" control={<Radio />} label="Very Much Like Me" />
+        <FormControlLabel value="2" control={<Radio />} label="Mostly Like Me" />
+        <FormControlLabel value="3" control={<Radio />} label="Somewhat Like Me" />
+        <FormControlLabel value="4" control={<Radio />} label="Not Much Like Me" />
+        <FormControlLabel value="5" control={<Radio />} label="Not Like Me At All" />
+      </RadioGroup>
+    </FormControl>
       </Box>
 
       <Box
         components="span"
         sx={{
           px: 10,
-          py: 5,
+          py: 2,
           width: '100%',
           height: '5%',
         }}
+      ><FormControl>
+      <FormLabel id="demo-controlled-radio-buttons-group">3.I have been obsessed with a certian idea or project for a short time but later lost interest. </FormLabel>
+      <RadioGroup
+        aria-labelledby="demo-controlled-radio-buttons-group"
+        name="controlled-radio-buttons-group"
+    
       >
-        {' '}
-        2. Setbacks do NOT discourage me.
-        <Slider
-          // aria-label="Restricted values"
-          defaultValue={50}
-          step={null}
-          marks={marks}
-          valueLabelDisplay="off"
-          track={false}
-        />
+        <FormControlLabel value="1" control={<Radio />} label="Very Much Like Me" />
+        <FormControlLabel value="2" control={<Radio />} label="Mostly Like Me" />
+        <FormControlLabel value="3" control={<Radio />} label="Somewhat Like Me" />
+        <FormControlLabel value="4" control={<Radio />} label="Not Much Like Me" />
+        <FormControlLabel value="5" control={<Radio />} label="Not Like Me At All" />
+      </RadioGroup>
+    </FormControl>
       </Box>
 
       <Box
         components="span"
         sx={{
           px: 10,
-          py: 5,
+          py: 2,
           width: '100%',
           height: '5%',
         }}
+      ><FormControl>
+      <FormLabel id="demo-controlled-radio-buttons-group">4. I am a hardworker. </FormLabel>
+      <RadioGroup
+        aria-labelledby="demo-controlled-radio-buttons-group"
+        name="controlled-radio-buttons-group"
+    
       >
-        {' '}
-        3. I have been obsessed with a certian idea or project for a short time
-        but later lost interest.
-        <Slider
-          // aria-label="Restricted values"
-          defaultValue={50}
-          step={null}
-          marks={marks}
-          valueLabelDisplay="off"
-          track={false}
-        />
+        <FormControlLabel value="1" control={<Radio />} label="Very Much Like Me" />
+        <FormControlLabel value="2" control={<Radio />} label="Mostly Like Me" />
+        <FormControlLabel value="3" control={<Radio />} label="Somewhat Like Me" />
+        <FormControlLabel value="4" control={<Radio />} label="Not Much Like Me" />
+        <FormControlLabel value="5" control={<Radio />} label="Not Like Me At All" />
+      </RadioGroup>
+    </FormControl>
       </Box>
 
       <Box
         components="span"
         sx={{
           px: 10,
-          py: 5,
+          py: 2,
           width: '100%',
           height: '5%',
         }}
+      ><FormControl>
+      <FormLabel id="demo-controlled-radio-buttons-group">5. I often set goals but later choose to pursue a different one. </FormLabel>
+      <RadioGroup
+        aria-labelledby="demo-controlled-radio-buttons-group"
+        name="controlled-radio-buttons-group"
+    
       >
-        {' '}
-        4. I am a hard worker.
-        <Slider
-          // aria-label="Restricted values"
-          defaultValue={50}
-          step={null}
-          marks={marks}
-          valueLabelDisplay="off"
-          track={false}
-        />
+        <FormControlLabel value="1" control={<Radio />} label="Very Much Like Me" />
+        <FormControlLabel value="2" control={<Radio />} label="Mostly Like Me" />
+        <FormControlLabel value="3" control={<Radio />} label="Somewhat Like Me" />
+        <FormControlLabel value="4" control={<Radio />} label="Not Much Like Me" />
+        <FormControlLabel value="5" control={<Radio />} label="Not Like Me At All" />
+      </RadioGroup>
+    </FormControl>
       </Box>
 
       <Box
         components="span"
         sx={{
           px: 10,
-          py: 5,
+          py: 2,
           width: '100%',
           height: '5%',
         }}
+      ><FormControl>
+      <FormLabel id="demo-controlled-radio-buttons-group">6. I have a difficulty maintaining my focus on projects that take more than a few months to complete. </FormLabel>
+      <RadioGroup
+        aria-labelledby="demo-controlled-radio-buttons-group"
+        name="controlled-radio-buttons-group"
+    
       >
-        {' '}
-        5. I often set a goal but later choose to pursue a different one.
-        <Slider
-          // aria-label="Restricted values"
-          defaultValue={50}
-          step={null}
-          marks={marks}
-          valueLabelDisplay="off"
-          track={false}
-        />
+        <FormControlLabel value="1" control={<Radio />} label="Very Much Like Me" />
+        <FormControlLabel value="2" control={<Radio />} label="Mostly Like Me" />
+        <FormControlLabel value="3" control={<Radio />} label="Somewhat Like Me" />
+        <FormControlLabel value="4" control={<Radio />} label="Not Much Like Me" />
+        <FormControlLabel value="5" control={<Radio />} label="Not Like Me At All" />
+      </RadioGroup>
+    </FormControl>
       </Box>
 
       <Box
         components="span"
         sx={{
           px: 10,
-          py: 5,
+          py: 2,
           width: '100%',
           height: '5%',
         }}
+      ><FormControl>
+      <FormLabel id="demo-controlled-radio-buttons-group">7. I finish whatever I begin. </FormLabel>
+      <RadioGroup
+        aria-labelledby="demo-controlled-radio-buttons-group"
+        name="controlled-radio-buttons-group"
+    
       >
-        {' '}
-        6. I have difficulty maintaining focus on projects that take more than a
-        few months to complete.
-        <Slider
-          // aria-label="Restricted values"
-          defaultValue={50}
-          step={null}
-          marks={marks}
-          valueLabelDisplay="off"
-          track={false}
-        />
+        <FormControlLabel value="1" control={<Radio />} label="Very Much Like Me" />
+        <FormControlLabel value="2" control={<Radio />} label="Mostly Like Me" />
+        <FormControlLabel value="3" control={<Radio />} label="Somewhat Like Me" />
+        <FormControlLabel value="4" control={<Radio />} label="Not Much Like Me" />
+        <FormControlLabel value="5" control={<Radio />} label="Not Like Me At All" />
+      </RadioGroup>
+    </FormControl>
       </Box>
 
       <Box
         components="span"
         sx={{
           px: 10,
-          py: 5,
+          py: 2,
           width: '100%',
           height: '5%',
         }}
+      ><FormControl>
+      <FormLabel id="demo-controlled-radio-buttons-group">8. I am diligent </FormLabel>
+      <RadioGroup
+        aria-labelledby="demo-controlled-radio-buttons-group"
+        name="controlled-radio-buttons-group"
+    
       >
-        {' '}
-        7. I finish whatever I begin.
-        <Slider
-          // aria-label="Restricted values"
-          defaultValue={50}
-          step={null}
-          marks={marks}
-          valueLabelDisplay="off"
-          track={false}
-        />
-      </Box>
+        <FormControlLabel value="1" control={<Radio />} label="Very Much Like Me" />
+        <FormControlLabel value="2" control={<Radio />} label="Mostly Like Me" />
+        <FormControlLabel value="3" control={<Radio />} label="Somewhat Like Me" />
+        <FormControlLabel value="4" control={<Radio />} label="Not Much Like Me" />
+        <FormControlLabel value="5" control={<Radio />} label="Not Like Me At All" />
+      </RadioGroup>
+    </FormControl>
+      </Box> 
 
-      <Box
-        components="span"
-        sx={{
-          px: 10,
-          py: 5,
-          width: '100%',
-          height: '5%',
-        }}
-      >
-        {' '}
-        8. I am diligent.
-        <Slider
-          // aria-label="Restricted values"
-          defaultValue={50}
-          step={null}
-          marks={marks}
-          valueLabelDisplay="off"
-          track={false}
-        />
-      </Box>
-    </Container>
+    </Container> 
   );
 }
